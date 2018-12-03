31f1fa124831eb4da39c024ff72d81fc4df93293
Arjun Raja <arjun1997birju@gmail.com>
2018-12-03 12:44:05 +0000

    added database func

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost:27017/YelpCamp", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/YelpCamp", { useNewUrlParser: true });
+mongoose.connect("mongodb://Arjun:birju1997@ds123834.mlab.com:23834/campchamp", { useNewUrlParser: true });
+//
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
